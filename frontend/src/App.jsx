f79b3446a7294693cb48d4a318a4d031c6dbd016
Mohanraj111789 <kit27.am30@gmail.com>
2025-11-03 11:59:46 +0530

    frontend file upload frontend created un App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import "./App.css";
+
 const App = () => {
     return (
         <div>
-            <h1>File Upload</h1>
+            <div className="upload-container">
+                <h1>File Upload choose file</h1>
+                <div className="upload-input">
+                    <input type="file" id="file" name="file" />
+                    <label htmlFor="file" className="upload-label">Browse</label>
+                </div>
+            </div>
         </div>
     );
 };
